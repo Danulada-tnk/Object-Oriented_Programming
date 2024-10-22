3ce24e0692a0cd64a39364d07024c83281672d7c
Danulada-tnk <67319010027@tnk.ac.th>
2024-10-22 17:00:54 +0700

    area

---
+++ oop_week2/area.py
@@ -0,0 +1,17 @@
+print("โปรแกรมหาพื้นที่ 4 เหลี่ยมจตุรัส")  
+length = int(input('ด้าน: '))  
+area = length * length
+print(f'ค่าพื้นที่ 4 เหลี่ยม: {area} ตารางเมตร')  
+
+print("โปรแกรมหาพื้นที่ 4 เหลี่ยมผืนผ้า") 
+length = int(input('ใส่ความยาว (เมตร): ')) 
+width = int(input('ใส่ความกว้าง (เมตร): ')) 
+area = length * width  
+print(f'ค่าพื้นที่ 4 เหลี่ยมผืนผ้า: {area} ตารางเมตร')  
+
+print("โปรแกรมหาพื้นที่วงกลม")  
+radius = float(input('ใส่ค่ารัศมี (เมตร): ')) 
+pi = 3.14  
+area = pi * radius ** 2  
+print(f'ค่าพื้นที่วงกลม: {area:.2f} ตารางเมตร')  
+
