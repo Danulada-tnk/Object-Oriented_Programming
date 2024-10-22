cf664f331bf2eb613495c73d30a4d93ca63fecda
Danulada-tnk <67319010027@tnk.ac.th>
2024-10-22 16:27:39 +0700

    resume_V2

---
+++ oop_week2/resume_v2.py
@@ -0,0 +1,19 @@
+
+name = input("กรุณาป้อนชื่อจริง: ")
+print(name)
+student_id = input("กรุณาป้อนรหัสประจำตัวนักศึกษา: ")
+print(student_id)
+year = input("กรุณาป้อนชั้นปี: ")
+print(year)
+nickname = input("กรุณาป้อนชื่อเล่น: ")
+print(nickname)
+height = float(input("กรุณาป้อนส่วนสูง (เมตร): "))
+print(height)
+weight = float(input("กรุณาป้อนน้ำหนัก (กิโลกรัม): "))
+print(weight)
+total = height + weight
+
+print(f"ชื่อ: {name} | รหัส: {student_id}")
+print(f"ชั้นปี: {year}")
+print(f"ชื่อเล่น: {nickname}")
+print(f"สูง: {height} เมตร | น้ำหนัก: {weight} กิโลกรัม")
